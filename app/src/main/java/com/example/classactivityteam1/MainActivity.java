0c3da53485908577b7a2e4f95041a8fe437736da
1893888 <jatinjoshi2283@gmail.com>
2019-07-09 13:40:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/classactivityteam1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.classactivityteam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this, Menu.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
