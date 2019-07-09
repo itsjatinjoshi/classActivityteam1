171dc3b215a53e4f86b7686f39bded538b6269d1
Parwinder <sunnyjasti15@gmail.com>
2019-07-09 14:14:06 -0400

    animation done

--- app/src/main/java/com/example/classactivityteam1/MainActivity.java
+++ app/src/main/java/com/example/classactivityteam1/MainActivity.java
@@ -6,13 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView splashscreen;
+
     private final int SPLASH_DISPLAY_LENGTH = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        splashscreen= findViewById(R.id.splashscreen);
+
+
+        Animation anni= AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate);
+        anni.setDuration(5000);
+
+        splashscreen.setAnimation(anni);
+
+
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
